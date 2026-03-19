1d930eb53467e1ec6c679311e4d9aa71f7e16019
ceverett312 <ceverett312@gmail.com>
2026-03-18 22:31:41 -0400

    Add files via upload

---
+++ create_users.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Script to create users
+#takes input file with user names on the command line as the first arguement.
+
+#user name file 
+USERFILE=$1
+
+if [ "$USERFILE" = " " ] 
+then
+	echo "Please specify an input file!"
+	exit 10 
+elif test -e $USERFILE
+then
+	for user in  `cat $USERFILE`
+	do
+	echo "creating the "$user" user..."
+		useradd -m $user
+	done
+	exit 20
+else
+	echo " Invalid input file specified! "
+	exit 30
+fi 
