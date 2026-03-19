c9e2c01b1ab1c644115387343abaeeae9a48eb3f
ceverett312 <ceverett312@gmail.com>
2026-03-18 22:38:09 -0400

    Create delete_users.sh

---
+++ delete_users.sh
@@ -0,0 +1,24 @@
+#vi delete_users.sh
+
+#!/bin/bash
+Script to delete users 
+#takes input file with usernames on the command line as first arguement. 
+# user name file
+USERFILE=$1
+
+if [ "$USERFILE"="" ] 
+ then
+	echo "Please specify an input file!"
+	exit 10 
+elif test -e $USERFILE
+then
+	for user in  `cat $USERFILE`
+	do
+	echo "deleting the "$user" user..."
+		userdel -r $user
+	done
+	exit 20
+else
+	echo " Invalid input file specified! "
+	exit 30
+fi 
